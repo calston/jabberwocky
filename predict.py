d63d47019bbc9f1b617b335a83a49465e4e2ec3b
Colin Alston <colin.alston@gmail.com>
2012-09-26 19:37:54 +0200

    Impelement costs

--- predict.py
+++ predict.py
@@ -4,15 +4,29 @@ def sigmoid(z):
     # Sigmoid function 
     return 1.0 / (1 + np.exp(-z))
 
+def lrCost(theta, X, y, l):
+    # Logistic regression cost function
+    rTheta = theta
+    rTheta[0] = 0
+    m = len(y)
+
+    hx = sigmoid(X.dot(theta));
+
+    return 1/m * (-y.transpose()*np.log(hx) - (1 - y.transpose())*np.log(1 - hx)) + (l/(2*m))*(rTheta**2).sum()
 
 def minimiseLRF(X, y, l):
-    # Minimuses logistic sigmoid function to theta
+    # Minimuses logistic sigmoid function to theta for each discrete set
     labels = y.max()+1
     
     m, n = X.shape
 
-    theta = np.zeroes((lables, n + 1))
+    theta = np.zeros((labels, n + 1))
     X = np.hstack([np.ones((m, 1)), X])
 
     for i in range(labels):
+        initial_t = np.zeros((n + 1, 1))
         
+        lcJ = lambda t: lrCost(t, X, y, 0.1)
+
+        # ... where's my minimizorz :( 
+        optimize.fmin_cg(lcJ, initial_t, maxiter=100)
--- test.py
+++ test.py
@@ -1,5 +1,23 @@
-import readDigits
+import readDigits, predict
+
+import numpy as np
 
 X,y = readDigits.loadSet('digits.jpg', 10, 5, "72104149590690159734966540740131347271211792351244")
-print X
-print y
+
+print len(y)
+
+labels = y.max()+1
+
+m, n = X.shape
+
+theta = np.zeros((labels, n + 1))
+X = np.hstack([np.ones((m, 1)), X])
+
+initial_t = np.zeros((n + 1, 1))
+
+J, grad= predict.lrCost(initial_t, X, y, 0.1)
+
+print J.shape, J
+
+print grad.shape, grad
+
