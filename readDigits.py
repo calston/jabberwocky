8668ef10fb527db921284c959d2fb9ba4cd7ea79
Colin Alston <colin.alston@gmail.com>
2012-09-27 19:04:00 +0200

    And some comments might help

--- readDigits.py
+++ readDigits.py
@@ -35,25 +35,46 @@ def loadSet(filename, w, h, vals):
 
     return X, numpy.array([int(i) for i in vals])
 
+
 def findParent(x, y, features):
-    # Find aproximately neighbouring pixels in the features set
+    # Find aproximately neighboring pixels in the features set
+    # and return the parents of those neighbors
     found = []
     for i in range(-2,3):
         for j in range(-2,3):
             # Run through every permutation of the nearest nodes
             tpl = (x+i, y+j)
-            # Return the parent for the neighbour node
+            # Return the parent for the neighbor node
             f = features.get(tpl, None)
             if f and (f not in found):
                 found.append(f)
     return found
 
 
-def isolateDigits(filename, tollerance=20, correctionFactor = 5, showTest = False):
-    """ A very very suboptimal quick and dirty feature detection
-        using scanlines and a tree and some solidly unfounded 
-        principals """
+def isolateDigits(filename, contrastMin=100, contrastMax=256, tollerance=20, correctionFactor = 5, showTest = False):
+    """Isolates and extracts features from an image.
+            
+            The image is reduced to scan lines. On each line points are
+            found in the intensity range from contrastMin to contrastMax. 
+            For each new point found, a map of connected points is created 
+            back to the first parent in the feature 
+                point => parent(neighbor)
+            The parent is also mapped back to child points
+                parent => [c1, c2, ..., cn]
+            If a point is found to have multiple neighbors which 
+            resolve to multiple parents, the first parent chosen 
+            to replace all other parents thus merging the tree for that feature.
+            (the parent=>child reverse map speeds this up) 
+            Once the image is scanned, the map is reversed creating new map
+            of parents to sets of X and Y values the children occupied
+            The minima and maxima of these sets define the bounding box of the 
+            feature. 
 
+        tollerance - The minimum area something must be bound by to be 
+                     considered a feature
+        correctionFactor - Additional area around the feature bounding
+        showTest - Display some useful test imagary of what happened
+    """
     im = ImageOps.invert(Image.open(filename)).convert('L')
     w, h = im.size
     x, y = (0,1)
@@ -68,10 +89,10 @@ def isolateDigits(filename, tollerance=20, correctionFactor = 5, showTest = Fals
     for val in st:
         x+= 1
         # Find bright pixels
-        point = val > 0
+        point = (val >= contrastMin) and (val <= contrastMax)
 
         if point:
-            # Find neighbouring pixels
+            # Find neighboring pixels
             feature = findParent(x,y, features)
             if feature:
                 # Index the parent feature to this feature tree
---
+++ test2.py
@@ -0,0 +1,5 @@
+import readDigits, predict
+
+import numpy as np
+
+blocks = readDigits.isolateDigits('samples/sample1.png', showTest = True)
